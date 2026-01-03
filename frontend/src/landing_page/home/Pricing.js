185f0ec898c6e390af7bdb6e1bb59334649ca079
= <=>
2026-01-03 14:09:59 +0530

    Add frontend React app files

---
+++ frontend/src/landing_page/home/Pricing.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+function Pricing() {
+  return ( 
+    <h2>Pricing</h2>
+   );
+}
+
+export default Pricing;
---
+++ frontend/src/landing_page/products/ProductsPage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Navbar from '../Navbar';
+import Hero from './Hero';
+import RightSection from './RightSection';
+import LeftSection from './LeftSection';
+import Universe from './Universe';
+import Footer from '../Footer';
+
+function PricingPage() {
+  return ( 
+    <>
+     <Navbar/>
+     <Hero/>
+     <RightSection/>
+     <LeftSection/>
+    <Universe/>
+     <Footer/>
+    </>
+   );
+}
+
+export default PricingPage;
